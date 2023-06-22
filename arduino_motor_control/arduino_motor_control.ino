901dc26d913fa518258e9d15fd6a3299f5e869bb
Branko Trifkovic <brankotrifkovic2@gmail.com>
2023-06-22 17:13:53 +0200

    Stepper motor control

--- arduino_motor_control/arduino_motor_control.ino
+++ arduino_motor_control/arduino_motor_control.ino
@@ -1,34 +1,34 @@
-#include <Servo.h>
+#include <AFMotor.h>
 
-char command;
-Servo servo;
+#define MOTOR_SPEED 50
+#define MOTOR_STEP 200
+#define DELAY 2000 // milliseconds
+
+// Number of steps per output rotation
+// Change this as per your motor's specification
+const int stepsPerRevolution = 200;
+
+// connect motor to port #2 (M3 and M4)
+AF_Stepper motor(stepsPerRevolution, 2);
+
+
+void serial_flush();
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  servo.attach(3);
+  motor.setSpeed(MOTOR_SPEED);  // 50 rpm   
 }
-
 
 void loop() {
 
   if(Serial.available()) {
-    command = Serial.read();
-
-    switch(command) {
-      case 'R':
-        servo.write(45);
-        break;
-      case 'V':
-        servo.write(90);
-        break;
-      case 'O':
-        servo.write(90);
-        break;
-      default:
-        servo.write(150);
-    }
-    
+    motor.step(MOTOR_STEP, FORWARD, SINGLE);
+    delay(DELAY);
+    serial_flush();
   }
+    
 }
 
+void serial_flush() {
+  while(Serial.available()) Serial.read();
+}
--- yolov8/real_time.py
+++ yolov8/real_time.py
@@ -6,8 +6,8 @@ import time
 
 mode = 1 # 0-train, 1-detect
 model = YOLO('C:/Users/tbran/Desktop/banana_detection_model/script/runs/detect/train6/weights/best.pt')
-#class_id = ["OverRipe", "Ripe", "VeryRipe"]
-class_id = ['O', 'R', 'V']
+class_id = ["OverRipe", "Ripe", "VeryRipe"]
+#class_id = ['O', 'R', 'V']
 
 if __name__ == "__main__":
     arduino = serial.Serial(port='COM4', baudrate=9600, bytesize=8)
@@ -21,19 +21,18 @@ if __name__ == "__main__":
 
         while (True):
 
-
-
             ret, frame = video.read()
-            results = model(frame)[0]
+            results = model(frame, conf=0.6)[0]
             detections = sv.Detections.from_yolov8(results)
 
             # Extracting confidence score and class id from above results
             results_list = results.boxes.data.tolist()
             for data in results_list:
-                #print("Confidence score: " + str(data[4]))
-                #print("Class: " + banana_class)
-                banana_class = class_id[int(data[5])]
-                arduino.write(banana_class.encode())
+                print(data)
+                if int(data[5]) != 1:
+                    command = '1'
+                    arduino.write(command.encode())
+
 
             # Drawing bounding boxes on the frame
             frame = bounding_box_annotator.annotate(scene=frame, detections=detections)
@@ -47,6 +46,6 @@ if __name__ == "__main__":
         # Close serial port
         arduino.close()
         # After the loop release the cap object
-        vid.release()
+        video.release()
         # Destroy all the windows
         cv2.destroyAllWindows()
